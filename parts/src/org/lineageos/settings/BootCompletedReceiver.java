53b03168ce49e5760a976086e849d6a2e0153738
Sergey Vasiliev <sdv22@live.ru>
2023-06-25 15:00:17 +0300

    Baikalify XiaomiParts (just deisable unused services)

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -32,7 +32,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
-        ThermalUtils.startService(context);
-        RefreshUtils.initialize(context);
+        //ThermalUtils.startService(context);
+        //RefreshUtils.initialize(context);
     }
 }
